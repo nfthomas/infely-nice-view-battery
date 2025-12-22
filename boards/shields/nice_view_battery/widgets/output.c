b70141bc34fb71be6d87341929913e49f8c7d181
nfthomas <70170157+nfthomas@users.noreply.github.com>
2025-12-22 15:51:23 -0700

    Move connection bar to top; extend to full height of icon

--- boards/shields/nice_view_battery/widgets/output.c
+++ boards/shields/nice_view_battery/widgets/output.c
@@ -19,38 +19,35 @@
 static void draw_usb_connected(lv_obj_t *canvas) {
     lv_draw_rect_dsc_t rect_dsc;
     init_rect_dsc(&rect_dsc, LVGL_FOREGROUND);
-    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y + 27, 68, 13, &rect_dsc);
+    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y, 68, 22, &rect_dsc);
 
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
-    img_dsc.zoom = (13 << 8) / 22;
 
-    lv_canvas_draw_img(canvas, OFFSET_X + 27, OFFSET_Y + 27, &usb, &img_dsc);
+    lv_canvas_draw_img(canvas, OFFSET_X + 23, OFFSET_Y, &usb, &img_dsc);
 }
 
 static void draw_ble_unbonded(lv_obj_t *canvas) {
     lv_draw_rect_dsc_t rect_dsc;
     init_rect_dsc(&rect_dsc, LVGL_BACKGROUND);
-    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y + 27, 68, 13, &rect_dsc);
+    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y, 68, 22, &rect_dsc);
 
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
-    img_dsc.zoom = (13 << 8) / 22;
 
-    lv_canvas_draw_img(canvas, OFFSET_X + 27, OFFSET_Y + 27, &unbound, &img_dsc);
+    lv_canvas_draw_img(canvas, OFFSET_X + 23, OFFSET_Y, &unbound, &img_dsc);
 }
 #endif
 
 static void draw_ble_disconnected(lv_obj_t *canvas) {
     lv_draw_rect_dsc_t rect_dsc;
     init_rect_dsc(&rect_dsc, LVGL_BACKGROUND);
-    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y + 27, 68, 13, &rect_dsc);
+    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y, 68, 22, &rect_dsc);
 
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
-    img_dsc.zoom = (13 << 8) / 22;
 
-    lv_canvas_draw_img(canvas, OFFSET_X + 27, OFFSET_Y + 27,
+    lv_canvas_draw_img(canvas, OFFSET_X + 23, OFFSET_Y,
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
                         &bt_disconnected,
 #else
@@ -62,13 +59,12 @@ static void draw_ble_disconnected(lv_obj_t *canvas) {
 static void draw_ble_connected(lv_obj_t *canvas) {
     lv_draw_rect_dsc_t rect_dsc;
     init_rect_dsc(&rect_dsc, LVGL_FOREGROUND);
-    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y + 27, 68, 13, &rect_dsc);
+    lv_canvas_draw_rect(canvas, OFFSET_X, OFFSET_Y, 68, 22, &rect_dsc);
 
     lv_draw_img_dsc_t img_dsc;
     lv_draw_img_dsc_init(&img_dsc);
-    img_dsc.zoom = (13 << 8) / 22;
 
-    lv_canvas_draw_img(canvas, OFFSET_X + 27, OFFSET_Y + 27,
+    lv_canvas_draw_img(canvas, OFFSET_X + 23, OFFSET_Y,
 #if !IS_ENABLED(CONFIG_ZMK_SPLIT) || IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
                         &bt,
 #else
